16a4040242daa9d41bc28811de09d862105d5217
David Stearns <dlsinfo@uw.edu>
2013-10-30 11:52:06 -0700

    A few more points and some user feedback in the demo (html5 scripting)

--- notes/adv-js/js/demo.js
+++ notes/adv-js/js/demo.js
@@ -1,15 +1,18 @@
 
 $(function(){
     $('.btn-geoloc').click(function(){
-        if (navigator && navigator.geolocation)
+        if (navigator && navigator.geolocation) {
+            $(this).html('Getting your location...');
             navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError, 
                 {enableHighAccuracy: true});
+        }
         else
             alert("Your browser doesn't appear to support geolocation!");
     });
 });
 
 function onGeoSuccess(position) {
+    $('.btn-geoloc').html("Try It Again!");
     alert("Your current coordinates are latitude=" 
         + position.coords.latitude 
         + ", longitude="
@@ -17,5 +20,6 @@ function onGeoSuccess(position) {
 }
 
 function onGeoError(err) {
+    $('.btn-geoloc').html("Sorry, Geolocation Unavailable");
     alert("Sorry, I was unabe to obtain your coordinates for the following reason: " + err.message);
 }
